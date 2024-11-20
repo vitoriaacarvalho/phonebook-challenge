33ce5638cac0362c9f81f2f0e8886bc5e8858569
Vitoria <vitoriaprogramadora@gmail.com>
2024-11-20 15:50:11 -0300

    add service to call backend apis

---
+++ frontend/src/services/contact/contact.service.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosError } from "axios";
+import { Contact } from "./contact.dto";
+
+const api = axios.create({
+  baseURL: `http://localhost:4000/api`,
+});
+
+const onRejected = async (error: AxiosError) => {
+  console.log(error);
+};
+
+export const getContacts = async (): Promise<Contact[]> => {
+  const response = await api.get("/contacts");
+  return response.data.body;
+};
+
+export const addContact = async (contact: Contact) => {
+  const response = await api.post("/contacts", contact);
+  return response.data;
+};
+
+export const deleteContact = async (id: string) => {
+  const response = await api.delete(`/contacts/${id}`);
+  return response.status;
+};
+
+export const updateContact = async (id: string, contact: Contact) => {
+  const response = await api.put(`/contacts/${id}`, contact);
+  return response.data;
+};
+
+api.interceptors.response.use((response) => response, onRejected);
