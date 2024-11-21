fa1bc3c045e6869610caa645c8683a93af122d4d
Vitoria <vitoriaprogramadora@gmail.com>
2024-11-21 00:17:58 -0300

    update backend port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import router from './routes/ContactApiRoutes';
 import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './swagger'; 
+import swaggerSpec from './swagger';
+import cors from 'cors'; 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 4000;
+app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(express.json());
